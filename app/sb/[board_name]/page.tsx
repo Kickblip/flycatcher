56337aa0b0f997a3321dcf162128345a322ad6f9
Kickblip <wyatt.zilker@gmail.com>
2024-06-19 11:03:24 -0500

    add loading wheel to board page

--- app/sb/[board_name]/page.tsx
+++ app/sb/[board_name]/page.tsx
@@ -1,13 +1,16 @@
 "use client"
 
+import LoadingWheel from "@/components/dashboard/LoadingWheel"
 import { useEffect, useState } from "react"
 
 export default function BoardInfo({ params }: { params: { board_name: string } }) {
   const [error, setError] = useState<string | null>(null)
+  const [loading, setLoading] = useState(true)
   const [board, setBoard] = useState<any>(null) // TODO: Define types
 
   const fetchBoard = async () => {
     setError(null)
+    setLoading(true)
 
     try {
       const response = await fetch(`/api/pub/boards/${params.board_name}`, {
@@ -17,18 +20,25 @@ export default function BoardInfo({ params }: { params: { board_name: string } }
       if (!response.ok) {
         const errorData = await response.json()
         throw new Error(errorData.message || "Board does not exist")
+        setLoading(false)
       }
 
       const data = await response.json()
       setBoard(data)
+      setLoading(false)
     } catch (error) {
       setError((error as Error).message || "Board does not exist")
+      setLoading(false)
     }
   }
 
   useEffect(() => {
     fetchBoard()
   }, [params.board_name])
+
+  if (loading) {
+    return <LoadingWheel />
+  }
 
   if (error) {
     return <div className="text-red-500">{JSON.stringify(board)}</div>
