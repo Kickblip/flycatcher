6c7d658f7c15182391c80d1b06444fdf1971afcd
Kickblip <wyatt.zilker@gmail.com>
2024-06-18 21:27:15 -0500

    trig redeploy

--- components/suggestionsform/suggestionform.tsx
+++ components/suggestionsform/suggestionform.tsx
@@ -23,86 +23,82 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/suggestionsform/form"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/suggestionsform/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/suggestionsform/card"
 import { Input } from "@/components/suggestionsform/input"
 import { ChevronDown, ChevronUp, SendIcon } from "lucide-react"
 
 const formSchema = z.object({
-  title: z.string().min(2, {
-    message: "Title must be more than 2 characters."
-  }).max(30, {
-    message: "Title can't be more than 30 characters."
-  }), //change depending
+  title: z
+    .string()
+    .min(2, {
+      message: "Title must be more than 2 characters.",
+    })
+    .max(50, {
+      message: "Title can't be more than 50 characters.",
+    }), //change depending
   description: z.string().min(2).max(100),
 })
 
 const formSchemaComment = z.object({
-  comment: z.string().min(2, {
-    message: "Comment must be more than 2 characters."
-  }).max(300, {
-    message: "Comment can't be more than 300 characters."
-  }), //change depending
+  comment: z
+    .string()
+    .min(2, {
+      message: "Comment must be more than 2 characters.",
+    })
+    .max(300, {
+      message: "Comment can't be more than 300 characters.",
+    }), //change depending
 })
 
 interface Board {
-  _id: string;
-  name: string;
-  urlName: string;
-  primaryColor: string;
-  secondaryColor: string;
-  accentColor: string;
-  textColor: string;
+  _id: string
+  name: string
+  urlName: string
+  primaryColor: string
+  secondaryColor: string
+  accentColor: string
+  textColor: string
   suggestions: {
-    title: string;
-    description: string;
-    votes: number;
-    comments: string[];
-  }[];
+    title: string
+    description: string
+    votes: number
+    comments: string[]
+  }[]
 }
 
 interface BoardProps {
-  board: Board | null; 
+  board: Board | null
 }
 
+export default function Suggestion({ board }: BoardProps) {
+  const [suggestions, setSuggestions] = useState<Board["suggestions"]>([]) // cahnge names
 
-export default function Suggestion({board}: BoardProps)  {
-  const [suggestions, setSuggestions] = useState<Board["suggestions"]>([]); // cahnge names
-
-  
   useEffect(() => {
     if (board) {
       //sets the suggestions
       setSuggestions(board.suggestions)
 
       //sets the buttons
-      document.documentElement.style.setProperty('--primary', board.accentColor);
-      document.documentElement.style.setProperty('--primary-foreground', board.primaryColor);
+      document.documentElement.style.setProperty("--primary", board.accentColor)
+      document.documentElement.style.setProperty("--primary-foreground", board.primaryColor)
 
       //sets the background and text
-      document.documentElement.style.setProperty('--card', board.primaryColor);
-      document.documentElement.style.setProperty('--card-foreground', board.textColor);
+      document.documentElement.style.setProperty("--card", board.primaryColor)
+      document.documentElement.style.setProperty("--card-foreground", board.textColor)
 
-      //sets the popover 
-      document.documentElement.style.setProperty('--popover', board.secondaryColor);
-      document.documentElement.style.setProperty('--popover-foreground', board.textColor);
+      //sets the popover
+      document.documentElement.style.setProperty("--popover", board.secondaryColor)
+      document.documentElement.style.setProperty("--popover-foreground", board.textColor)
     }
   }, [board])
 
   const suggestionForm = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema)
+    resolver: zodResolver(formSchema),
   })
 
   const commentForm = useForm<z.infer<typeof formSchemaComment>>({
-    resolver: zodResolver(formSchemaComment)
-  });
-
+    resolver: zodResolver(formSchemaComment),
+  })
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     const newSuggestion = { ...values, votes: 0, comments: [] }
@@ -110,29 +106,27 @@ export default function Suggestion({board}: BoardProps)  {
     suggestionForm.reset()
   }
 
-
   //thank you chatGPT for solving this. this has to be the most crazy solution
   function onClickVoteUp(index: number, increment: number = 1) {
     setSuggestions((prevSuggestions) =>
       prevSuggestions.map((suggestion, idx) =>
-        idx === index ? { ...suggestion, votes: suggestion.votes + increment } : suggestion
-      )
-    );
+        idx === index ? { ...suggestion, votes: suggestion.votes + increment } : suggestion,
+      ),
+    )
   }
 
   function onSubmitComment(index: number, comment: string) {
     setSuggestions((prevSuggestions) =>
       prevSuggestions.map((suggestion, idx) =>
-        idx === index ? { ...suggestion, comments: [...suggestion.comments, comment] } : suggestion
-      )
-    );
-    commentForm.reset();
+        idx === index ? { ...suggestion, comments: [...suggestion.comments, comment] } : suggestion,
+      ),
+    )
+    commentForm.reset()
   }
 
   if (!board) {
-    return <p>Loading...</p>; // or handle the loading state appropriately
+    return <p>Loading...</p> // or handle the loading state appropriately
   }
-
 
   return (
     <div className="flex space-x-4">
@@ -142,47 +136,44 @@ export default function Suggestion({board}: BoardProps)  {
         </CardHeader>
         <CardContent>
           <Form {...suggestionForm}>
-          <form onSubmit={suggestionForm.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={suggestionForm.control}
-              name="title"
-              defaultValue=""
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Suggestion Title</FormLabel>
-                  <FormControl>
-                    <Input 
-                    placeholder="Add a Darkmode" {...field} 
-                    style={{ backgroundColor: board.secondaryColor}}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={suggestionForm.control}
-              name="description"
-              defaultValue=""
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Suggestion</FormLabel>
-                  <FormControl>
-                  <Textarea
-                      placeholder="Please add a dark mode as my eyes burn looking at your website!"
-                      {...field}
-                      rows={5} 
-                      style={{ backgroundColor: board.secondaryColor}}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="flex justify-center">
-              <Button type="submit">Submit</Button>
-            </div>
-          </form>
+            <form onSubmit={suggestionForm.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={suggestionForm.control}
+                name="title"
+                defaultValue=""
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Suggestion Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Add a Darkmode" {...field} style={{ backgroundColor: board.secondaryColor }} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={suggestionForm.control}
+                name="description"
+                defaultValue=""
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Suggestion</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Please add a dark mode as my eyes burn looking at your website!"
+                        {...field}
+                        rows={5}
+                        style={{ backgroundColor: board.secondaryColor }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-center">
+                <Button type="submit">Submit</Button>
+              </div>
+            </form>
           </Form>
         </CardContent>
       </Card>
@@ -191,35 +182,21 @@ export default function Suggestion({board}: BoardProps)  {
           <CardTitle>Suggestions</CardTitle>
         </CardHeader>
         <CardContent>
-        {suggestions.length > 0 ? (
+          {suggestions.length > 0 ? (
             suggestions.map((suggestion, index) => (
-              <Card 
-              key={index} 
-              className="mt-4 relative"
-              style={{ backgroundColor: board.secondaryColor }}
-              >
+              <Card key={index} className="mt-4 relative" style={{ backgroundColor: board.secondaryColor }}>
                 <CardHeader>
                   <CardTitle>{suggestion.title}</CardTitle>
                 </CardHeader>
                 <div className="absolute top-0 right-0 flex items-center">
                   <p className="mr-2">{suggestion.votes}</p>
                   <div className="flex flex-col">
-                  <Button 
-                    variant="ghost" 
-                    size="icon" 
-                    className="h-7 w-7"
-                    onClick={() => onClickVoteUp(index, 1)}
-                    >
-                    <ChevronUp className="h-4 w-4" />
-                  </Button>
-                  <Button 
-                    variant="ghost" 
-                    size="icon" 
-                    className="h-7 w-7" 
-                    onClick={() => onClickVoteUp(index, -1)}
-                    >
+                    <Button variant="ghost" size="icon" className="h-7 w-7" onClick={() => onClickVoteUp(index, 1)}>
+                      <ChevronUp className="h-4 w-4" />
+                    </Button>
+                    <Button variant="ghost" size="icon" className="h-7 w-7" onClick={() => onClickVoteUp(index, -1)}>
                       <ChevronDown className="h-4 w-4" />
-                  </Button>
+                    </Button>
                   </div>
                 </div>
                 <CardContent>
@@ -228,35 +205,23 @@ export default function Suggestion({board}: BoardProps)  {
                     <div className="flex justify-end">
                       <DialogTrigger>Open</DialogTrigger>
                     </div>
-                    <DialogContent className="sm:max-w-[1000px]" style={{ backgroundColor: board.primaryColor}}>
+                    <DialogContent className="sm:max-w-[1000px]" style={{ backgroundColor: board.primaryColor }}>
                       <DialogHeader>
                         <DialogTitle>{suggestion.title}</DialogTitle>
-                        <DialogDescription>
-                          {suggestion.description}
-                        </DialogDescription>
+                        <DialogDescription>{suggestion.description}</DialogDescription>
                       </DialogHeader>
                       <div className="absolute top-1 right-10 flex items-center">
                         <p className="mr-2">{suggestion.votes}</p>
                         <div className="flex flex-col">
-                        <Button 
-                          variant="ghost" 
-                          size="icon" 
-                          className="h-7 w-7"
-                          onClick={() => onClickVoteUp(index, 1)}
-                          >
-                          <ChevronUp className="h-4 w-4" />
-                        </Button>
-                        <Button 
-                          variant="ghost" 
-                          size="icon" 
-                          className="h-7 w-7" 
-                          onClick={() => onClickVoteUp(index, -1)}
-                          >
+                          <Button variant="ghost" size="icon" className="h-7 w-7" onClick={() => onClickVoteUp(index, 1)}>
+                            <ChevronUp className="h-4 w-4" />
+                          </Button>
+                          <Button variant="ghost" size="icon" className="h-7 w-7" onClick={() => onClickVoteUp(index, -1)}>
                             <ChevronDown className="h-4 w-4" />
-                        </Button>
+                          </Button>
                         </div>
                       </div>
-                      <br/>
+                      <br />
                       <p>Comments</p>
                       {suggestion.comments && suggestion.comments.length > 0 ? (
                         suggestion.comments.map((comment, commentIndex) => (
@@ -270,27 +235,31 @@ export default function Suggestion({board}: BoardProps)  {
                         <p>No comments yet.</p>
                       )}
                       <Form {...commentForm}>
-                            <form onSubmit={commentForm.handleSubmit((values) => onSubmitComment(index, values.comment))} className="space-y-8">
-                              <FormField
-                                control={commentForm.control}
-                                name="comment"
-                                defaultValue=""
-                                render={({ field }) => (
-                                  <FormItem>
-                                    <FormLabel>Your comment</FormLabel>
-                                    <FormControl>
-                                      <Input 
-                                      placeholder="Type your comment here" {...field} 
-                                      style={{ backgroundColor: board.secondaryColor}}
-                                      />
-                                    </FormControl>
-                                    <FormMessage />
-                                  </FormItem>
-                                )}
-                              />
-                              <Button type="submit">Submit</Button>
-                            </form>
-                          </Form>
+                        <form
+                          onSubmit={commentForm.handleSubmit((values) => onSubmitComment(index, values.comment))}
+                          className="space-y-8"
+                        >
+                          <FormField
+                            control={commentForm.control}
+                            name="comment"
+                            defaultValue=""
+                            render={({ field }) => (
+                              <FormItem>
+                                <FormLabel>Your comment</FormLabel>
+                                <FormControl>
+                                  <Input
+                                    placeholder="Type your comment here"
+                                    {...field}
+                                    style={{ backgroundColor: board.secondaryColor }}
+                                  />
+                                </FormControl>
+                                <FormMessage />
+                              </FormItem>
+                            )}
+                          />
+                          <Button type="submit">Submit</Button>
+                        </form>
+                      </Form>
                     </DialogContent>
                   </Dialog>
                 </CardContent>
@@ -299,10 +268,8 @@ export default function Suggestion({board}: BoardProps)  {
           ) : (
             <p>No suggestions yet.</p>
           )}
-          </CardContent>
+        </CardContent>
       </Card>
     </div>
-
-
   )
 }
